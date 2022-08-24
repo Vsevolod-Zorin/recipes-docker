ac579e0d294bcaca82b15ed3df1173f095b7b4e1
Vsevolod Zorin <work.vsevolodzorin@gmail.com>
2022-08-24 09:09:41 +0300

    api tests init

---
+++ api/src/__tests__/category.test.ts
@@ -0,0 +1,8 @@
+describe('category', () => {  describe('get category route', () => {
+    describe('given the category does not exist', () => {
+      it('should return a 404', async () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});

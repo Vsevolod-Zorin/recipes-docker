56c6936d8edc9f5eda3d99028ccfdf90c25b5543
Vsevolod Zorin <work.vsevolodzorin@gmail.com>
2022-09-05 12:20:47 +0300

    frontend: config proj

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import App from "./components/App";
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import App from 'src/components/App';
+import './index.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<React.StrictMode>{<App />}</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function

a364d76ee7cf96acef507eeeecfd7152a65cc5f0
Vsevolod Zorin <work.vsevolodzorin@gmail.com>
2022-09-07 08:40:28 +0300

    dash: new before editor

--- dashboard/src/routes/index.tsx
+++ dashboard/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Categories from 'src/containers/Categories';
 import Home from 'src/containers/Home';
 import Post from 'src/containers/Post';
 import Posts from 'src/containers/Posts';
@@ -18,24 +19,15 @@ export const CustomRouter = () => {
 	return (
 		<Routes>
 			<Route path="/">
-				{/* <Route path="/" element={<Home />} /> */}
-				<Route path={routersPaths.category}>
-					{/* <Route path={routersPaths.category} element={<Categories />} />
-					<Route path="editor" element={<CategoryEditor />} />
-					<Route path="editor/:id" element={<CategoryEditor />} /> */}
-				</Route>
-				<Route path={routersPaths.recipe}>
-					<Route path={routersPaths.recipes} element={<Recipes />} />
-					{/* <Route path="editor" element={<RecipeEditor />} />
-					<Route path="editor/:id" element={<RecipeEditor />} /> */}
-				</Route>
-				<Route path="post">
-					<Route path={routersPaths.posts} element={<Posts />} />
-					{/* <Route path="editor" element={<PostEditor />} /> */}
-					{/* <Route path="editor/:id" element={<PostEditor />} /> */}
-				</Route>
+				<Route path="/" element={<Home />} />
+				<Route path={routersPaths.categories} element={<Categories />} />
+				<Route path={routersPaths.recipes} element={<Recipes />} />
+				<Route path={routersPaths.posts} element={<Posts />} />
+				<Route path={routersPaths.categories + '/editor/:id'} element={<Home />} />
+				<Route path={routersPaths.recipes + '/editor/:id'} element={<Home />} />
+				<Route path={routersPaths.posts + '/editor/:id'} element={<Home />} />
 			</Route>
-			<Route path="*" element={<Navigate to="/category" replace />} />
+			<Route path="*" element={<Navigate to="/" replace />} />
 		</Routes>
 	);
 };

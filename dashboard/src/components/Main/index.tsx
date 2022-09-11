28eb507c604faa384ae4ba52bbe57c47a2e75f4a
Vsevolod Zorin <work.vsevolodzorin@gmail.com>
2022-09-11 12:25:10 +0300

    dach: add custom scroll

--- dashboard/src/components/Main/index.tsx
+++ dashboard/src/components/Main/index.tsx
@@ -6,12 +6,12 @@ import './main.scss';
 
 const Main = () => {
 	return (
-		<main className="main">
-			<div className="main-wrapper content">
-				<section className="main__left-part">
+		<main className="main custom-scroll">
+			<div className="main-wrapper content ">
+				<section className="main__left-part custom-scroll">
 					<Aside />
 				</section>
-				<section className="main__right-part">
+				<section className="main__right-part custom-scroll">
 					{/* <Breadcrumbs /> */}
 					<div className="router-wrapper">
 						<CustomRouter />
